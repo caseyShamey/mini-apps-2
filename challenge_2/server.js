9b0e293d9d0d17173c6476d41a65d584d0e32753
Casey Shamey <caseyshamey@gmail.com>
2019-04-01 22:01:49 -0400

    Complete setup webpack, babel, react, express, node

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(`${__dirname}/client/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 
